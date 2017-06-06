743b4ec5b578e54c0b4c1e27d02d034db3b1b939
nahal <mirzanahal@gmail.com>
2017-06-06 05:15:31 +0430

    chera tamum nmishe :(((((((

--- src/soccer/data/PlayerRepo.java
+++ src/soccer/data/PlayerRepo.java
@@ -60,6 +60,9 @@ public class PlayerRepo {
                         break;
                     }
             }
+            rs.close();
+            s.close();
+            c.close();
 
         }
         catch(Exception e) {
@@ -69,9 +72,60 @@ public class PlayerRepo {
         return result;
     }
 
+    public List<Player> getAll(){
+        List<Player> result = new ArrayList<>();
+        try{
+            Connection c = ConnectionFactory.getConnection();
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery("select * from person p , player plyr where p.name = plyr.name");
+            while (rs.next()){
+                Player player =new Player (rs.getString("name"),
+                        rs.getString("expertise"),
+                        rs.getInt("amadegi"),
+                        rs.getInt("ghodrat_badani"),
+                        rs.getInt("ghodrat_pass"),
+                        rs.getInt("toop_giri"),
+                        rs.getInt("ghodrat_golzani"),
+                        rs.getInt("ghodrat_shoot"),
+                        rs.getInt("sorat"),
+                        rs.getInt("darvazebani") );
+
+                player.setPrice(rs.getInt("price"));
+                result.add(player);
+            }
+            rs.close();
+            s.close();
+            c.close();
+
+        }catch (Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        return result;
+    }
+
     public List<Player> getAvailables(Integer year) {
-        // #TODO implemnet
-        return new ArrayList<>();
+        List<Player> players = getAll();
+        try{
+            Connection c = ConnectionFactory.getConnection();
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery("select name from player, contract where person = name AND year ="+year);
+
+            while(rs.next()){
+                String name = rs.getString("player");
+                for(Player player : players)
+                    if(player.getName().equals(name)){
+                        players.remove(player);
+                        break;
+                    }
+            }
+            rs.close();
+            s.close();
+            c.close();
+
+        }catch (Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        return players;
     }
 
     public void updateRolesAndShirts(List<Player> players, String team) {
@@ -88,9 +142,13 @@ public class PlayerRepo {
                                 "set shirt_number = "+player.getShirtNumber()+
                                 ", role = \'" + player.getRole() + "\' " +
                                 "where player = \'"+ player.getName() + "\'");
+                        s2.close();
                         break;
                     }
             }
+            rs.close();
+            s.close();
+            c.close();
 
         }catch (Exception e){
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
